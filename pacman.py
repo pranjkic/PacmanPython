fac7ea31e9adc390d5c5b2c6266b0f01ec4d49de
Marko <Marko@DESKTOP-H1LI6NS>
2019-12-04 19:22:18 +0100

    Ispravka pacman.py

--- pacman.py
+++ pacman.py
@@ -1,44 +1,17 @@
-# ovo ce nam biti main
-from PyQt5.QtWidgets import *
-
-import sys
-from PyQt5.QtWidgets import QWidget, QDesktopWidget, QApplication
-from PyQt5.QtGui import QIcon
-
-
-class MainWindow(QMainWindow, QWidget):
-
-    def __init__(self):
-        super().__init__()
-
-        self.init()
-
-    def init(self):
-        self.resize(600, 650)
-        self.center()
-        self.setWindowTitle("PAC-MAN")
-        self.seticon()
-        self.show()
-
-    def center(self):
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
-
-    def seticon(self):
-        self.setWindowIcon(QIcon('pm.png'))
-
-    def closeEvent(self, event):
-        reply = QMessageBox.question(self, 'Message', "Are you sure to quit?",
-                                      QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
-        if reply == QMessageBox.Yes:
-            event.accept()
-        else:
-            event.ignore()
-
+import  pygame
+from drawing import *
+import time
+def startApp():
+    screen = pygame.display.set_mode([606, 606])
+    pygame.display.set_caption('Pac-Man')
+    all_sprites_list = pygame.sprite.RenderPlain()
+    wall_list = setupRoomOne(all_sprites_list)
+    wall_list.draw(screen)
+    all_sprites_list.draw(screen)
+    pygame.display.flip()
+    time.sleep(5)
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    sys.exit(app.exec_())
+    pygame.init()
+    startApp()
+    pygame.quit()

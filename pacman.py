3664c6e596b636e1041046d13da00f1c78efeac4
pranjkic <m.pranjkic@gmail.com>
2019-12-10 15:56:11 +0100

    Iscrtavanje pacman ikonice.

--- pacman.py
+++ pacman.py
@@ -1,15 +1,20 @@
-import  pygame
+import pygame
 from drawing import *
 import time
+from iconsdrawing import *
+
+
 def startApp():
     screen = pygame.display.set_mode([606, 606])
     pygame.display.set_caption('Pac-Man')
     all_sprites_list = pygame.sprite.RenderPlain()
+    setupIcons(all_sprites_list)
     wall_list = setupRoom(all_sprites_list)
     wall_list.draw(screen)
     all_sprites_list.draw(screen)
     pygame.display.flip()
     time.sleep(5)
+
 
 if __name__ == '__main__':
     pygame.init()

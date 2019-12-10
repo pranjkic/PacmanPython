2016d71ffb23d4879f5362933e1a332f6faf5476
Marko <Marko@DESKTOP-H1LI6NS>
2019-12-10 17:39:58 +0100

    Dodavanje figurica, i nove lokacije

--- iconsdrawing.py
+++ iconsdrawing.py
@@ -11,10 +11,18 @@ def setupIcons(all_sprites_list):
     pacman_collide.add(Pacman)
 
     monsta_list = pygame.sprite.RenderPlain()
-    Blinky = Ghost(w, b_h, "images/Blinky.png")
+    Blinky = Ghost(w - 30, b_h, "images/Blinky.png")
     monsta_list.add(Blinky)
     all_sprites_list.add(Blinky)
 
-    Pinky = Ghost(w - 30, m_h, "images/Pinky.png")
+    Pinky = Ghost(w, m_h, "images/Pinky.png")
     monsta_list.add(Pinky)
     all_sprites_list.add(Pinky)
+
+    Inky = Ghost(w + 30, i_h, "images/Inky.png")
+    monsta_list.add(Inky)
+    all_sprites_list.add(Inky)
+
+    Clyde = Ghost(w, c_h, "images/Clyde.png")
+    monsta_list.add(Clyde)
+    all_sprites_list.add(Clyde)
--- location.py
+++ location.py
@@ -1,4 +1,6 @@
 w = 303 - 16  # Width
 p_h = (7 * 60) + 19
 b_h = (3 * 60) + 70  # Binky height
-m_h = (4 * 60) + 25  # Pinky height
+m_h = (4 * 60) - 10  # Pinky height
+i_h = (3 * 60) + 70  # Inky height
+c_h = (4 * 60) + 25  # Clyde height

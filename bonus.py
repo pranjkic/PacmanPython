0fbe996e1ad2af32adbb919fe74c6c4e51ce8ab9
pranjkic <m.pranjkic@gmail.com>
2019-12-26 12:49:38 +0100

    Dodavanje bonusa na git.

---
+++ bonus.py
@@ -0,0 +1,14 @@
+import pygame
+
+
+class Cherry(pygame.sprite.Sprite):
+
+    def __init__(self, x, y, filename):
+
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load(filename).convert()
+
+        self.rect = self.image.get_rect()
+        self.rect.top = y
+        self.rect.left = x
